e20a868a5332a511a0c973ee9d027c549584fa97
Samin Sarker <saminsarker05@gmail.com>
2024-01-17 00:49:26 -0500

    working on mineEther endpoint

--- EtherChain.py
+++ EtherChain.py
@@ -10,12 +10,17 @@ class EtherBlock:
     self.transactions = transactions
     self.proof = proof
     self.previous_hash = previous_hash
+  
+  def __str__(self):
+    return f"Index: {self.index}, Timestamp: {self.timestamp}, Transactions: {self.transactions}, Proof: {self.proof}, Previous Hash: {self.previous_hash}"
 
 
 class EtherChain:
   def __init__(self):
     self.chain = LinkedList()
     self.transactions = LinkedList()
+
+    self.new_block(proof = 100, previous_hash = 1)
 
   def new_transaction(self, sender, recipient, amount):
     data = {
@@ -24,7 +29,8 @@ class EtherChain:
       'amount': amount
     }
     self.transactions.append(data)
-    return self.last_block['index'] + 1
+    print(self.last_block())
+    return self.last_block().data.index + 1
 
   def new_block(self, proof, previous_hash = None):
     block = EtherBlock(len(self.chain) + 1, self.transactions, proof, previous_hash)
@@ -32,11 +38,11 @@ class EtherChain:
     self.chain.append(block)
     return block
 
-  def last_block(self):
+  def last_block(self,):
     return self.chain.last()
 
-  def hash(EtherBlock):
-    pass
+  def hash(self, block):
+    return hashlib.sha256(str(block)).hexdigest()
   
   def poW(self, last_proof):
     proof = 0
--- app.py
+++ app.py
@@ -1,21 +1,42 @@
 from EtherChain import *
-
 from flask import Flask, request
 
 app = Flask(__name__)
+etherChain = EtherChain()
+
 
 @app.route('/mine', methods=['GET'])
 def mineEther():
-  return "mining..."
+  last_block = etherChain.last_block().data
+  last_proof = last_block.proof
+  new_proof = etherChain.poW(last_block)
+
+  etherChain.new_transaction(0, "Jack", 1)
+  previous_hash = etherChain.hash(last_block)
+
+  block = etherChain.new_block(new_proof, previous_hash)
+
+  response = {'message': f'new block {block.index} forged'}
+  return response
+
 
 @app.route('/chain', methods=['GET'])
 def seeChain():
   return "chain..."
 
+
 @app.route('/new', methods=['POST'])
 def transaction():
   data = request.get_json()
-  return data
+  required = ['sender', 'recipient', 'amount']
+  for i in data:
+    if i not in required:
+      print("missing values")
+      return 'Missing Values', 400
+  
+  index = etherChain.new_transaction(data['sender'], data['recipient'], data['amount'])
+  response = {'message': f'transaction will be added to ether block {index}'}
+  return response
 
 
 if __name__ == '__main__':
